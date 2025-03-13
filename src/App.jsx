ee77f958b0bc0129a2c14719e4a8231ed93d947b
gustavoweb <contato@gustavoweb.com>
2025-03-13 11:06:05 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Teste no React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+
+function Header(){
+    return(
+        <>
+            <header>
+                <h1>Valorant LP</h1>
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Caracteristicas</a></li>
+                        <li><a href="#">Contato</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+};
+export default Header;
